b9d0280d9609184884bf377c63eb43e583bbe7f3
Gustavo Neves <gustavonpr19@gmail.com>
2025-05-19 12:58:43 -0300

    Alterando os caminhos dos Modelos de Processamento LNP

--- Natural-Language-Processing/app/app.py
+++ Natural-Language-Processing/app/app.py
@@ -1,13 +1,24 @@
-
+import os
+import urllib.request
 import streamlit as st
 import joblib
 from utils import preprocess_text
 
 
 # Carregar arquivos necessários
-model = joblib.load("Natural Language Processing\models\extra_tress.pkl")
-vectorizer = joblib.load("Natural Language Processing\models\model_vectorizer_tfidef.pkl")
-label_encoder = joblib.load("Natural Language Processing\models\label_encoder.pkl")
+
+MODEL_PATH = "models/extra_tress.pkl"
+MODEL_URL = "https://drive.google.com/uc?export=download&id=1HO8VPMqEGOATPbw1GTIZOKlNBEOdbWMm"
+
+# Verifica se o modelo já está salvo localmente
+if not os.path.exists(MODEL_PATH):
+    os.makedirs("models", exist_ok=True)
+    urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+
+# Carrega o modelo
+model = joblib.load(MODEL_PATH)
+vectorizer = joblib.load("models/model_vectorizer_tfidef.pkl")
+label_encoder = joblib.load("models/label_encoder.pkl")
 
 # Interface do usuário
 st.set_page_config(page_title="Análise de Sentimentos no Twitter", layout="centered")
